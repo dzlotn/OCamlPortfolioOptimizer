38f5f6b3251dff001bac479a2d05776f56e645a2
Daniel Zlotnick <daniel.zlotnick5@gmail.com>
2025-12-02 16:39:34 -0500

    added usage instructions and main gui wrapper to run the gui

---
+++ bin/main_gui.ml
@@ -0,0 +1,16 @@
+open Lwt.Syntax
+open Questionnaire_types
+open Optimizer
+
+(* GUI version - separate executable with GUI support *)
+let run_gui_safe () =
+  try Gui.run_gui () with
+  | exn ->
+      Printf.eprintf
+        "Error starting GUI: %s\nPlease ensure SDL2 and tsdl are properly \
+         installed:\n  opam install tsdl\n"
+        (Printexc.to_string exn);
+      exit 1
+
+let () = run_gui_safe ()
+
